a120726a63ae143a1f71f644d71c0819ed890a12
Mohit Kumar <mohitkr7982@gmail.com>
2024-06-26 21:10:20 +0530

    added create token and verify token function in user service

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,4 +4,5 @@ const bcrypt=require('bcrypt')
 module.exports={
     PORT:process.env.PORT,
     SALT:++process.env.SALT,
+    JWT_KEY:process.env.JWT_KEY
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,8 @@
 
+const { JWT_KEY } = require('../config/serverConfig');
 const UserRepository=require('../repository/user-repository');
+var jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 class userService{
     constructor(){
@@ -13,6 +16,34 @@ class userService{
         }catch(error){
             console.log("something went wrong in user Service");
             throw error;
+        }
+    }
+
+    createToken(user){ 
+        try{
+            var token=jwt.sign(user,JWT_KEY,{ expiresIn: '1h' })
+            return token;
+        }catch(error){
+            console.log("something went wrong in createToken in userService", error)
+            throw error
+        }
+    }
+    verifyToken(token){        
+        try{
+            const response=jwt.verify(token,JWT_KEY)
+            return response;
+        }catch(error){
+            console.log("something went wrong in verifyToken in userService", error)
+            throw error
+        }
+    }
+
+    checkPassword(userPlanePassword,encrytPassword){
+        try{
+            return bcrypt.compareSync(userPlanePassword,encrytPassword)
+        }catch(error){
+            console.log("something went wrong in verifyToken in userService", error)
+            throw error
         }
     }
 
